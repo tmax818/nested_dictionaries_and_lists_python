602ab81f23460270757344f06f24be0a24b77a86
tmax818 <tylermaxwell661@gmail.com>
2022-10-24 17:34:02 -0700

    update sol branch

--- main.py
+++ main.py
@@ -3,11 +3,19 @@
 # TODO Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
 x = [[5, 2, 3], [10, 8, 9]]
 
+x[1][0] = 15
+
+print(x)
+
 # TODO Change the last_name of the first student from 'Jordan' to 'Bryant'
 students = [
     {'first_name': 'Michael', 'last_name': 'Jordan'},
     {'first_name': 'John', 'last_name': 'Rosales'}
 ]
+
+students[0]['last_name'] = 'Bryant'
+
+print(students)
 
 # TODO In the sports_directory, change 'Messi' to 'Andres'
 sports_directory = {
@@ -15,8 +23,15 @@ sports_directory = {
     'soccer': ['Messi', 'Ronaldo', 'Rooney']
 }
 
+sports_directory['soccer'][0] = 'Andres'
+
+print(sports_directory)
 # TODO Change the value 20 in z to 30
 z = [{'x': 10, 'y': 20}]
+
+z[0]['y'] = 30
+
+print(z)
 
 # TODO Iterate Through a List of Dictionaries Create a function iterateDictionary(some_list) that, given a list of
 #  dictionaries, the function loops through each dictionary in the list and prints each key and the associated value.
@@ -38,7 +53,12 @@ students = [
 # first_name - KB, last_name - Tonelcopy
 
 def iterateDictionary(studentsParameter):
-    pass
+    for student in studentsParameter:
+        for k, v in student.items():
+            print(f"{k} - {v}")
+
+
+iterateDictionary(students)
 
 
 # TODO Get Values From a List of Dictionaries
